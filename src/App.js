41b4b5c8bc4aed3e5d744210497d940255b9e30c
Techbite <bonfacemwema7@gmail.com>
2023-02-20 18:27:34 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello world!</h1>
+//     </div>
+//   );
+// }
 
-function App() {
+// const App = () => (
+//   <div>
+//     <h1>Hello world!</h1>
+//   </div>
+// );
+import React, { useCallback, useState, useEffect } from "react";
+
+const App = () => {
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onNewTodoChange = useCallback((event) => {
+    console.log(event.target.value);
+    setNewTodo(event.target.value);
+  },[]);
+
+  const formSubmitted = useCallback((event) => {
+    event.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        id: todos.length + 1,
+        content: newTodo,
+        done: false,
+      },
+    ]);
+    setNewTodo("");
+  }, [newTodo, todos]);
+
+  useEffect(() => {
+    console.log("todo", todos);
+  }, [todos]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={formSubmitted}>
+        <label htmlFor="newTodo">Enter a Todo:</label>
+        <input
+          id="newTodo"
+          name="newTodo"
+          value={newTodo}
+          type="text"
+          onChange={onNewTodoChange}
+        />
+        <button>Add Todo</button>
+      </form>
+      <ul>
+        {
+          todos.map((todo) => (
+            <li>{todo.content}</li>
+          ))
+        }
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
